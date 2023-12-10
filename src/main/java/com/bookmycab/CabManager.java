c7e1c2efa16049f7fe962e8c19799221cab858d1
virajb <virajb@zeta.tech>
2023-12-10 11:31:35 +0530

    pass cabIdleTimeIncreasePostRegistrationAsTimePasses

--- src/main/java/com/bookmycab/CabManager.java
+++ src/main/java/com/bookmycab/CabManager.java
@@ -5,11 +5,16 @@ import com.bookmycab.history.CabSnapshot;
 import com.bookmycab.time.Clock;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 public class CabManager {
     private LinkedList<String> idleCabList = new LinkedList<>();
     private LinkedList<String> onTripCabList = new LinkedList<>();
+
+    private final Map<String, LocalDateTime> cabLastIdleTime = new HashMap<>();
 
     private final Clock clock;
 
@@ -29,17 +34,21 @@ public class CabManager {
         if(idleCabList.isEmpty())
             throw new CabsNotAvailableException();
         String cabId = idleCabList.removeFirst();
+        cabLastIdleTime.remove(cabId);
         onTripCabList.add(cabId);
         return cabId;
     }
 
     public void register(String cabId) {
+        LocalDateTime now = clock.now();
+        cabLastIdleTime.put(cabId, now);
         idleCabList.add(cabId);
-        cabAuditor.record(new CabSnapshot(cabId, ""), clock.now());
+        cabAuditor.record(new CabSnapshot(cabId, ""), now);
     }
 
     public void endTrip(String cabId) {
         onTripCabList.remove(cabId);
+        cabLastIdleTime.put(cabId, clock.now());
         idleCabList.add(cabId);
     }
 
@@ -48,6 +57,6 @@ public class CabManager {
     }
 
     public Duration getIdleTime(String cabId) {
-        return Duration.ofMinutes(1);
+        return Duration.between(cabLastIdleTime.get(cabId), clock.now());
     }
 }
--- src/test/java/com/bookmycab/CabIdleTimeTest.java
+++ src/test/java/com/bookmycab/CabIdleTimeTest.java
@@ -1,26 +1,50 @@
 package com.bookmycab;
 
+import com.bookmycab.history.CabAuditor;
+import com.bookmycab.time.Clock;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.mockito.Mockito.doReturn;
 
 public class CabIdleTimeTest {
 
     @Test
     public void cabIdleFor1MinutePostRegistration() {
-        CabManager cabManager = new CabManager();
+        Clock mockClock = Mockito.mock(Clock.class);
+        final LocalDate FIXED_DATE = LocalDate.of(2019, 12, 31);
+        doReturn(LocalDateTime.of(FIXED_DATE, LocalTime.of(1, 0)))
+                .when(mockClock).now();
+        CabManager cabManager = new CabManager(new CabAuditor(), mockClock);
         cabManager.register("cab-1");
+
+        doReturn(LocalDateTime.of(FIXED_DATE, LocalTime.of(1, 1)))
+                .when(mockClock).now();
         assertThat(cabManager.getIdleTime("cab-1"), equalTo(Duration.ofMinutes(1)));
     }
 
     @Test
     public void cabIdleTimeIncreasePostRegistrationAsTimePasses() {
-        CabManager cabManager = new CabManager();
+        Clock mockClock = Mockito.mock(Clock.class);
+        final LocalDate FIXED_DATE = LocalDate.of(2019, 12, 31);
+        doReturn(LocalDateTime.of(FIXED_DATE, LocalTime.of(1, 0)))
+                .when(mockClock).now();
+        CabManager cabManager = new CabManager(new CabAuditor(), mockClock);
         cabManager.register("cab-1");
+
+        doReturn(LocalDateTime.of(FIXED_DATE, LocalTime.of(1, 1)))
+                .when(mockClock).now();
         assertThat(cabManager.getIdleTime("cab-1"), equalTo(Duration.ofMinutes(1)));
+
+        doReturn(LocalDateTime.of(FIXED_DATE, LocalTime.of(1, 2)))
+                .when(mockClock).now();
         assertThat(cabManager.getIdleTime("cab-1"), equalTo(Duration.ofMinutes(2)));
     }
 }
