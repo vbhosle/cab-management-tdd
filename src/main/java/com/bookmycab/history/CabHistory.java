b87859d26a0d8cfcefc474738fca02c9248ff9ed
virajb <virajb@zeta.tech>
2023-12-10 08:36:58 +0530

    pass cabHistoryIsEmptyForNonExistentCab

--- src/main/java/com/bookmycab/history/CabHistory.java
+++ src/main/java/com/bookmycab/history/CabHistory.java
@@ -1,9 +1,10 @@
 package com.bookmycab.history;
 
+import java.util.Collections;
 import java.util.List;
 
 public class CabHistory {
     public List<String> get(String cabId) {
-        return null;
+        return Collections.emptyList();
     }
 }
