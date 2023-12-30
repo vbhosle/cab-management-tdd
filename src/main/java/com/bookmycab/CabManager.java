2e91b3a6d865940c2cffff61017fc1647af17771
virajb <virajb@zeta.tech>
2023-12-30 21:57:03 +0530

    refactor methods that work with map of cab

--- src/main/java/com/bookmycab/CabManager.java
+++ src/main/java/com/bookmycab/CabManager.java
@@ -3,7 +3,7 @@ package com.bookmycab;
 import com.bookmycab.exception.CabNotAvailableException;
 
 import java.time.Duration;
-import java.util.Comparator;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,7 +16,11 @@ public class CabManager {
     }
 
     public void registerCab(String cabId, CabState cabState, String cityId) {
-        cabs.put(cabId, new CabSnapshot(cabId, cabState, cityId, clock.now()));
+        addOrReplaceCab(new CabSnapshot(cabId, cabState, cityId, clock.now()));
+    }
+
+    private void addOrReplaceCab(CabSnapshot cab) {
+        cabs.put(cab.getId(), cab);
     }
 
     public CabSnapshot getCab(String cabId) {
@@ -24,31 +28,35 @@ public class CabManager {
     }
 
     public CabSnapshot getCabForBooking(BookingCriteria bookingCriteria) {
-        return cabs.values().stream()
+        return getAllCabs().stream()
                 .filter(cabSnapshot -> cabSnapshot.getState() == CabState.IDLE)
                 .filter(cabSnapshot -> cabSnapshot.getCity().equals(bookingCriteria.getCity()))
                 .max((cab1, cab2) -> getCabIdleTime(cab1.getId()).compareTo(getCabIdleTime(cab2.getId())))
                 .orElseThrow(CabNotAvailableException::new);
     }
 
+    private Collection<CabSnapshot> getAllCabs() {
+        return cabs.values();
+    }
+
     public void changeCurrentCityOfCab(String cabId, String currentCity) {
-        CabSnapshot cabSnapshot = cabs.get(cabId);
-        cabs.put(cabId, cabSnapshot.withCurrentCity(currentCity));
+        CabSnapshot cabSnapshot = getCab(cabId);
+        addOrReplaceCab(cabSnapshot.withCurrentCity(currentCity));
     }
 
     public void updateCabToOnTrip(String cabId) {
-        CabSnapshot cabSnapshot = cabs.get(cabId);
-        cabs.put(cabId, cabSnapshot.onTrip(clock.now()));
+        CabSnapshot cabSnapshot = getCab(cabId);
+        addOrReplaceCab(cabSnapshot.onTrip(clock.now()));
     }
 
     public void updateCabToIdle(String cabId, String currentCityId) {
-        CabSnapshot cabSnapshot = cabs.get(cabId);
-        cabs.put(cabId, cabSnapshot.toIdle(currentCityId, clock.now()));
+        CabSnapshot cabSnapshot = getCab(cabId);
+        addOrReplaceCab(cabSnapshot.toIdle(currentCityId, clock.now()));
     }
 
     public Duration getCabIdleTime(String cabId) {
-        if(cabs.get(cabId).getState() == CabState.IDLE)
-            return Duration.between(cabs.get(cabId).getStateChangedAt(), clock.now());
+        if(getCab(cabId).getState() == CabState.IDLE)
+            return Duration.between(getCab(cabId).getStateChangedAt(), clock.now());
         return Duration.ZERO;
     }
 }
