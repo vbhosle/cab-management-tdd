c8012ebd9a3ecd1377865fcacda0173f82b88a5a
virajb <virajb@zeta.tech>
2023-12-30 11:47:45 +0530

    cosmetic changes to changeCurrentCityOfIdleCab

--- src/test/java/com/bookmycab/CabSnapshotTest.java
+++ src/test/java/com/bookmycab/CabSnapshotTest.java
@@ -12,9 +12,10 @@ public class CabSnapshotTest {
     public void changeCurrentCityOfIdleCab() {
         CabSnapshot cabSnapshot1 = new CabSnapshot("cab-1", IDLE, "city-1");
         CabSnapshot cabSnapshot2 = cabSnapshot1.withCurrentCity("city-2");
+
         assertNotSame(cabSnapshot1, cabSnapshot2);
-        assertThat(cabSnapshot2.getId(), is("cab-1"));
-        assertThat(cabSnapshot2.getState(), is(IDLE));
+        assertThat(cabSnapshot2.getId(), is(cabSnapshot1.getCity()));
+        assertThat(cabSnapshot2.getState(), is(cabSnapshot1.getState()));
         assertThat(cabSnapshot2.getCity(), is("city-2"));
     }
 }
